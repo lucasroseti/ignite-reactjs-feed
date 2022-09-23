9d422b0d01852081221d1e0399175d6c81146c76
Lucas Roseti <lc.roseti@gmail.com>
2022-09-23 00:01:40 -0300

    React Main Concepts (Components and Properties)
      - Default Exports vs Named Exports
      - CSS Modules, Scoped CSS, Box Model
      - Fonts (Roboto), Global CSS
      - Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-function App() {
+import { Header } from './components/Header'
+
+import './global.css'
+
+export function App() {
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   )
 }
-
-export default App
