8cb24ebf9a932b7f0f2fea1c4a0da74248ac2984
Lucas Roseti <lc.roseti@gmail.com>
2022-09-24 13:36:09 -0300

    Base Layout
      - aside e main in App structure
      - Scoped CSS in App
      - Sidebar construction
      - phosphor-react installed to get icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </div>
   )
 }
