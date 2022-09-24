65306ee279bcef89f324ebc82c7deef4e33f9c2b
Lucas Roseti <lc.roseti@gmail.com>
2022-09-24 16:13:20 -0300

    Post Component Construction
      - Tag Time
      - Header and content section in Post component
    
      - Color green variation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header'
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
@@ -11,7 +12,10 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   )
