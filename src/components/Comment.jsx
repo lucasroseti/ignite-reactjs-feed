73ef2080ed15705b34506125e75797916851bf0a
Lucas Roseti <lc.roseti@gmail.com>
2022-09-28 00:18:04 -0300

    Comment Component and Form in Post
      - Publish button with css condition to show only in focus
      - content in :before
      - default outline removed and box-shadow with color green-500 in focus
      - Color red-500

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/lucasroseti.png" className={styles.avatar} />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>User</strong>
+              <time title="24 de setembro às 15:10" dateTime="2022-09-24">Cerca de 1h atrás</time>
+            </div> 
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir
+            <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
